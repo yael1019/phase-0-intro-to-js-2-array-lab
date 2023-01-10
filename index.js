48ba4035d11c81366a7cf00b498d6dafa73ae6e1
Yael Harosh <yaelharosh01@gmail.com>
2023-01-10 13:01:32 -0500

    pass array lab

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
 // Write your solution here!
+const cats = ['Milo', 'Otis', 'Garfield'];
+
+function destructivelyAppendCat(name) {
+    return cats.push(name);
+}
+
+function destructivelyPrependCat(name) {
+    return cats.unshift(name);
+}
+
+function destructivelyRemoveLastCat() {
+    return cats.pop();
+}
+
+function destructivelyRemoveFirstCat() {
+    return cats.shift();
+}
+
+function appendCat(name) {
+    const newArr = [...cats, name];
+    return newArr;
+}
+
+function prependCat(name) {
+    const newArr = [name, ...cats];
+    return newArr;
+}
+
+function removeLastCat() {
+    return cats.slice(0, -1);
+}
+
+// console.log(removeLastCat());
+
+function removeFirstCat() {
+    return cats.slice(1);
+}
